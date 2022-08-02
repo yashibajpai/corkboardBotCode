3f5d6c80e8ccc9bb97f9a15518618bd83d70322f
yashibajpai <71567118+yashibajpai@users.noreply.github.com>
2022-08-01 20:27:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ sendall = 0
 
 # TODO 
 # replace the 0 with the pins channel ID for your sever
-pins_channel = 952666313787047977
+pins_channel =  [952666313787047977, 994937189395877909]
 
 # TODO
 # add any black listed channel IDs as a list separated by a comma (,)
